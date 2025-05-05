0cd77c8f38962d8dfedf87d07e8c84dedc56d64f
nandini2399 <nandinijairath@gmail.com>
2025-05-05 22:06:29 +0530

    Food App using React basic structure

--- App.js
+++ App.js
@@ -1,46 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1",{},"Hello world from React"); // I can pass attributes in the {} object  like id:"heading"
-// const parent = React.createElement("div",{id:"parent"},
-//     React.createElement("div",{id:"child"},
-//         [React.createElement("h1",{},"I am H1 Tag"),
-//         React.createElement("h2",{},"I am H2 Tag")]
-//     )
-// ); // I can pass attributes in the {} object  like id:"heading"
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-// console.log(parent)/
-
-//React Element
-const jsxHeading = <h1 className="head">Namaste React using React Element</h1>;
-
-const Title = () => {
-  return (
-    <div>
-      <h1 className="head">Namaste React 🚀 using React Component</h1>
-      
-    </div>
-  );
-};
-
-const NextTitle = () =>{
-    return <h2>Learning with Akshay is so much fun!! 😁</h2>
+const Header = () => {
+    return (
+        <div className="header">
+            <div><img className="logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2024/10/1/f6d6503d-bfbe-49ad-97bc-1a63a1f520c6_8378db10-8513-466a-8500-9548593934fe.jpg"></img></div>
+            <div className="nav-items">
+               <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>🛒</li>
+                </ul> 
+        </div>
+        </div>
+    )
 }
 
-//React Functional Component
-const HeadingComponent = () => {
-  return (
-    <div id="container">
-        <Title />
-        {Title()}
-        {console.log("Hello")}
-        <NextTitle />  
-      <h2 className="head">By Nandini Jairath</h2>
-    </div>
-  );
-};
+const resCardStyle = {
+    backgroundColor : "#f0f0f0"
+}
+
+const ResCard = () => {
+    return (
+        <div className="res-card" style={resCardStyle}>         
+            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/kf9bbtt8to6ceel0zyxp" />
+            <h3>Desi Vibes</h3>
+            <h4>North Indian, Asian</h4>
+            <h4>4.7 ⭐</h4>
+            <h4>24 minutes</h4>
+        </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            <ResCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (    
+        <div className="app">
+            <Header />
+            <Body />
+            
+        </div>
+        
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(jsxHeading)
-root.render(<HeadingComponent />); //This is the syntax that babel understands
+root.render(<AppLayout />); //This is the syntax that babel understands
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Namaste React 🚀
+
+echo "# FoodVerse" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nandini2399/FoodVerse.git
+git push -u origin main
